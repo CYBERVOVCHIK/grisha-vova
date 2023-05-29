ae9429941c019f2a85b94c08a096b2439a1b3d56
Vasyan <rewsbar@gmail.com>
2023-05-29 10:26:25 +0300

    Zdes byl Grisha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <h2>ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫыы</h2>
     </div>
   )
 }
