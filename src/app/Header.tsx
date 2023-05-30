c065f06ed4f0c37e1e4d5c35ef73eb2f1291a389
Vladimir <gagnguy@mail.ru>
2023-05-30 17:00:29 +0300

    шапка готова

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,16 +3,18 @@ import s from './header.module.css'
 
 export default function Header() {
   return (
-    <div className={s.header}>
-      <div className={s.title}>Республика Татарстан</div>
-      <div className={s.nav_items}>
+    <header className={s.header}>
+
+    <div className={s.title}>Республика Татарстан</div>
+      <div className={s.menu}>
         <ul className={s.nav_items}>
-          <li><a href='#' className={`${s.nav_item}`}><span className={s.active}>Карта ДТП</span></a></li>
+          <li><a href='#' className={s.nav_item}><span className={s.active}>Карта ДТП</span></a></li>
           <li><a href='#' className={s.nav_item}><span>История авторизаций</span></a></li>
         </ul>
-
-        <a href='#' className={s.quit}><span >Выйти</span></a>
       </div>
-    </div>
+
+      <a href='#' className={s.quit}><span >Выйти</span></a>
+
+    </header>
   )
 }
