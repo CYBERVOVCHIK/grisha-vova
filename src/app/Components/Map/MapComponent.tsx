c7e21853568f9b86dc4fbba771166d443ffc632a
Vladimir <gagnguy@mail.ru>
2023-05-31 12:00:13 +0300

    добавил карту, иконка маркера почему-то не отображается

--- src/app/Components/Map/MapComponent.tsx
+++ src/app/Components/Map/MapComponent.tsx
@@ -1,7 +1,14 @@
-"use client"
-import React, { useState } from 'react'
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
-import m from './MapComponent.module.css'
+"use client";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L, { icon } from "leaflet";
+import m from "./MapComponent.module.css";
+import "leaflet/dist/leaflet.css";
+
+const markerIcon = new L.Icon({
+  iconUrl: require("./location-pin.png"),
+  iconSize: [40, 40],
+});
 
 export default function MapComponent() {
   let total = 100;
@@ -11,22 +18,46 @@ export default function MapComponent() {
     <div className={m.map_container}>
       <span className={m.info}>Информация по комплексам</span>
       <div className={m.map_stat}>
-          <div className={m.complex_total}>
-            <span className={`${m.complex_span} ${m.complex_total}`}>Количество мест работы комплексов:</span>
-            <div className={`${m.number} ${m.total}`}>{total}</div>
-          </div>
-          <div className={m.complex_intouch}>
-            <span className={`${m.complex_span} ${m.intouch}`}>Количество комплексов вышедших сегдня на свзязь:</span>
-            <div className={`${m.number} ${m.intouch}`}>{intouch}</div>
-          </div>
-          <div className={m.complex_working}>
-            <span className={`${m.complex_span} ${m.working}`}>Работает в данный момент:</span>
-            <div className={`${m.number} ${m.working}`}>{working}</div>
-          </div>
+        <div className={m.complex_total}>
+          <span className={`${m.complex_span} ${m.complex_total}`}>
+            Количество мест работы комплексов:
+          </span>
+          <div className={`${m.number} ${m.total}`}>{total}</div>
+        </div>
+        <div className={m.complex_intouch}>
+          <span className={`${m.complex_span} ${m.intouch}`}>
+            Количество комплексов вышедших сегдня на свзязь:
+          </span>
+          <div className={`${m.number} ${m.intouch}`}>{intouch}</div>
+        </div>
+        <div className={m.complex_working}>
+          <span className={`${m.complex_span} ${m.working}`}>
+            Работает в данный момент:
+          </span>
+          <div className={`${m.number} ${m.working}`}>{working}</div>
+        </div>
       </div>
       <div className={m.map}>
-        
+        <MapContainer
+          className={m.leaflet_container}
+          center={[55.79114510998451, 49.11037307686192]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={[55.82313839924307, 49.09658950859296]}
+            icon={markerIcon}
+          >
+            <Popup>
+              <strong>Гриша</strong>
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
-  )
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Задание',
-  description: 'Made by Grisha and Vova',
-}
+  title: "Задание",
+  description: "Made by Grisha and Vova",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ru">
+      
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
